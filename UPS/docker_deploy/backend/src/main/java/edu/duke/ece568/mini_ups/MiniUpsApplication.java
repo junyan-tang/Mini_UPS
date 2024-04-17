d0ecbed52e20aadbb2d8e7d054ddda37c3fd3918
junyan <junyantang1008@gmail.com>
2024-04-17 14:01:12 -0400

    initial structure

---
+++ UPS/docker_deploy/backend/src/main/java/edu/duke/ece568/mini_ups/MiniUpsApplication.java
@@ -0,0 +1,13 @@
+package edu.duke.ece568.mini_ups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUpsApplication.class, args);
+	}
+
+}
---
+++ UPS/docker_deploy/backend/src/test/java/edu/duke/ece568/mini_ups/MiniUpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.duke.ece568.mini_ups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
