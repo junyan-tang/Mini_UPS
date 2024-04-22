ddd2f4010761500a7ea127fddea950f5de438948
junyan <junyantang1008@gmail.com>
2024-04-21 20:58:16 -0400

    change config

--- UPS/docker_deploy/backend/src/main/java/edu/duke/ece568/mini_ups/config/SecurityConfig.java
+++ UPS/docker_deploy/backend/src/main/java/edu/duke/ece568/mini_ups/config/SecurityConfig.java
@@ -1,31 +1,40 @@
 package edu.duke.ece568.mini_ups.config;
 
 import javax.sql.DataSource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
+            .authorizeHttpRequests(authz -> authz
+                .anyRequest().hasRole("USER")
+            )
+            .formLogin(form -> form
                 .loginPage("/login")
                 .defaultSuccessUrl("/home", true)
                 .permitAll()
-                .and()
-            .logout()
-                .permitAll();
+            )
+            .logout(logout -> logout.permitAll());
+        return http.build();
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) {
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
             .dataSource(dataSource)
             .usersByUsernameQuery("select username, password, enabled from users where username=?")
